cd88a34b6098e6efd7090155c13e7ab44a344777
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-03-23 20:16:29 -0300

    Conectado ao banco de dados.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const api = 'https://servicodados.ibge.gov.br/api/v1/produtos/'
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const a = async () => {
-    const response = await fetch(api)
-    const data = await response.json()
-    console.log(data)
-}
+const mongodb = require('mongoose')
 
-a()
+mongodb.connect('mongodb+srv://root:admin@cluster0.hs8z9ef.mongodb.net/').then(()=>{
+    console.log('conectado')
+}).catch(()=>{
+    console.log('error')
+})
+
+
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/cadastro',(req,res)=>{
+    res.render('cadastro')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.listen(3000,()=>{
+    console.log('http://localhost:3000')
+})
