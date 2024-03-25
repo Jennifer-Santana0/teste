d2987c4551eaf7bf6bf3413dc37b035f63f0ee07
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-03-25 19:18:25 -0300

    Modificando um pouco o arquivo main.js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,37 @@
-$(document).ready(function() {
-    $('.addToCartButton').click(function(event) {
-        event.preventDefault(); // Evita o comportamento padrão do botão
+const addCarts = document.querySelectorAll('.addToCartButton');
 
-        // Obtém os dados do produto para adicionar ao carrinho
-        var form = $(this).closest('form');
-        var action = form.attr('action');
-        var produto = action.split('/')[2]; // Obtém o nome do produto da rota
-        var preco = action.split('/')[3]; // Obtém o preço do produto da rota
-        var id_produto = action.split('/')[4]; // Obtém o ID do produto da rota
+addCarts.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
 
-        // Objeto contendo os dados do produto
-        var data = {
+        let form = button.closest('form');
+        let rota = form.getAttribute('action');
+        let segments = rota.split('/');
+
+
+        let produto = segments[2];
+        let preco = segments[3];
+        let id_produto = segments[4];
+
+        let data = {
             produto: produto,
             preco: preco,
             id_produto: id_produto
         };
 
-        // Envia uma solicitação POST AJAX para o servidor
-        $.ajax({
-            type: 'POST',
-            url: action, // Usa a mesma URL do formulário
-            data: data,
-            success: function(response) {
-                // Lidar com a resposta do servidor, se necessário
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', rota);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
                 console.log('Produto adicionado ao carrinho com sucesso');
-            },
-            error: function(xhr, status, error) {
-                // Lidar com erros, se houver
-                console.error('Erro ao adicionar produto ao carrinho:', error);
+            } else {
+                console.error('Erro ao adicionar produto ao carrinho:', xhr.statusText);
             }
-        });
+        };
+        xhr.onerror = function() {
+            console.error('Erro ao adicionar produto ao carrinho:', xhr.statusText);
+        };
+        xhr.send(JSON.stringify(data));
     });
 });
